937952759d885109ea540a2db62e4843445e5953
Seu nome <halisterfernando@hotmail.com>
2022-11-05 12:33:57 -0300

    chore: :art: criada estilização dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import styles from './App.module.css'
 
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main className={styles.main}>
+        <h1>Conteúdo...</h1>
+      </main>
       <Footer />
     </div>
   );
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-type Props = {}
+import styles from './Footer.module.css'
 
-const Footer = (props: Props) => {
+const Footer = () => {
   return (
-    <footer>
+    <footer className={styles.footer}>
         <p>
             <span>
                 React + TS Todo
