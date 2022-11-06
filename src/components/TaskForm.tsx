32bd12ce84eb122a36fc45580ea64450bbcb5367
Seu nome <halisterfernando@hotmail.com>
2022-11-06 10:04:23 -0300

    chore: :art: estiliza formulario

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,23 +1,19 @@
 import React from 'react'
-
+import styles from './TaskForm.module.css'
 type Props = {
     btnText: string
 }
 
 const TaskForm = ({btnText}: Props) => {
   return (
-    <form>
-        <div>
-            <label htmlFor="title">
-                Título:
+    <form className={styles.form}>
+        <div className={styles.input_container}>
+            <label htmlFor="title">Título:</label>
                 <input type="text" name='title' placeholder='Título da tarefa'/>
-            </label>
         </div>
-        <div>
-            <label htmlFor="difficulty">
-                Título:
+        <div className={styles.input_container}>
+            <label htmlFor="difficulty">Dificuldade:</label>
                 <input type="text" name='difficulty' placeholder='Dificuldade da tarefa'/>
-            </label>
         </div>
         <input type="submit" value={btnText}/>
     </form>
